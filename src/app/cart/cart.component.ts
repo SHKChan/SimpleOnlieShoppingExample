e05dc7b49f953219a0cd07a54f6cc4f17807d728
FelixC <g125553693@gmail.com>
2023-06-07 13:29:19 +1000

    Implement ngForm in different way

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../service/cart.service';
-import { FormBuilder } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -10,20 +10,17 @@ import { FormBuilder } from '@angular/forms';
 export class CartComponent {
   items = this.cartService.getItems();
 
-  checkoutForm = this.forBuilder.group({
-    name: '',
-    address: ''
-  });
+  deliverInfo = { name: '', address: '' };
 
   constructor(
-    private cartService: CartService,
-    private forBuilder: FormBuilder
+    private cartService: CartService
   ) { }
 
-  onSubmit(): void {
+  onSubmit(
+    deliveryForm: NgForm): void {
     // Process checkout data here
     this.items = this.cartService.clearCart();
-    console.warn('Your order has been submitted', this.checkoutForm.value);
-    this.checkoutForm.reset();
+    console.warn('Your order has been submitted', deliveryForm.value.name, deliveryForm.value.address);
+    deliveryForm.reset();
   }
 }
